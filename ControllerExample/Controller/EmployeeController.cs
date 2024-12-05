46e124fe02bf11ef0a0f5d21eb2f1b4fe87ab773
mahendrad11 <mahendernath.d@gmail.com>
2024-12-05 23:16:46 +0530

    dotcore project changes

---
+++ ControllerExample/Controller/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using ControllerExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
+
+namespace ControllerExample
+{
+    public class EmployeeController : Controller
+    {
+        [Route("Registration")]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            List<string> errorlist = new List<string>();
+            if (!ModelState.IsValid)
+            {
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errorlist.Add(error.ErrorMessage);
+                    }
+                }
+                var errors = string.Join("\n", errorlist);
+                return Json(errors);
+
+            }
+
+            return Json(employee);
+        }
+    }
+}
---
+++ ControllerExample/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ControllerExample.Models
+{
+
+    public class Person
+    {
+        public Guid Id1 { get; set; }
+        public int Id { get; set; }
+
+
+        //     [FromRoute]        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+
+   
+}
