0139745ac7a61286c56962f0efcab6ab7a9e4be7
mahendrad108 <mahendranathdudyala@gmail.com>
2024-12-08 23:04:02 +0530

    Update EmployeeController.cs once again

--- ControllerExample/Controller/EmployeeController.cs
+++ ControllerExample/Controller/EmployeeController.cs
@@ -8,7 +8,7 @@ namespace ControllerExample
     { 
         [Route("Registration")]
         public IActionResult AddEmployee(Employee employee)
-        {
+        { 
             List<string> errorlist = new List<string>();
             if (!ModelState.IsValid)
             {
